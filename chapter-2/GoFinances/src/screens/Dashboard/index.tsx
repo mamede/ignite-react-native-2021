c85e4c6fdeb662f8257a13df5e5659a9dc74b12a
Felipe Mamede <felipebrasilmv@gmail.com>
2022-04-09 22:25:27 -0300

    feat: criando componentes com styled components

---
+++ chapter-2/GoFinances/src/screens/Dashboard/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import * as Styled from './styles'
+
+export function Dashboard() {
+  return(
+    <Styled.Container>
+      <Styled.Title>Dashboard</Styled.Title>
+    </Styled.Container>
+  )
+}
